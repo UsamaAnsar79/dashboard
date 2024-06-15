952945bda3feffd3d3c1b89f786d34be99ec1432
Usama Ansar <140901613+UsamaAnsar79@users.noreply.github.com>
2024-06-15 11:56:27 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+import React from 'react';
+import Dashboard from './Dashboard';
+const App = () => {
+  return (
+    <div>
+ <Dashboard />
+    </div>
+  );
+};
+
+export default App;
